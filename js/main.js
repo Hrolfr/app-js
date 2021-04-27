d776131a770fcbd5476eaf9aa50dad9eb2e60d3a
Hrolfr <sabrina.sales163@gmail.com>
2021-04-27 11:49:57 -0300

    vars

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+var display = document.getElementById('display');
+
+var display = document.getElementById('minutos');
+var display = document.getElementById('segundos');
+var display = document.getElementById('comecar');
+
+var cronometroSeg;
+
+var minutoAtual;
+var segundoAtual;
+
+var interval;
