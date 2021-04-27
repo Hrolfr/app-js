f8d121420238b1c49ca0773e2ff4dc6f3b327e72
Hrolfr <sabrina.sales163@gmail.com>
2021-04-27 12:12:09 -0300

    event listener

--- js/main.js
+++ js/main.js
@@ -19,3 +19,34 @@ for(var i = 0; i <= 60; i++){
 for(var i = 1; i <= 60; i++){
     segundos.innerHTML += '<option value="' +i+ '">' +i+ ' </option>';
 }
+//começa de 1,pois segundos funcionam assim
+
+ comecar.addEventListener('click', function(){
+   minutoAtual = minutos.value;
+   segundoAtual = segundos.value;
+
+   display.childNodes[1].innerHTML = minutoAtual + ":"+segundoAtual;
+
+   interval = setInterval(function(){
+
+   segundoAtual--;
+   if(segundoAtual <= 0){
+       if (minutoAtual > 0){
+  
+          minutoAtual--;
+          segundoAtual = 59;
+       }else{
+           alert("acabou");
+           document.getElementById("sound").play();
+           clearInterval(interval);
+       }
+   }
+
+   },1000); //vai ser chamada de 1 em 1min
+
+ })
+
+ //com o value eu vou conseguir recuperar o que está selecionado nos itens
+ /*ele ouxa o display,chama o childNodes no índice 1.
+ ChildNodes no JS vai pegar todos os elementos filhos;Como só tenho 1 aqui,chamo no índice 1 e então to pegando o h3.
+ Depois chamo o innerHTML e coloco o valor de forma dinamica que será atualizado com o evento setinterval*/
