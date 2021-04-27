b61f5bf2dbd2638e9393f16b0a71cb5aac1cb78d
Hrolfr <sabrina.sales163@gmail.com>
2021-04-27 12:00:02 -0300

    segundos configurados

--- js/main.js
+++ js/main.js
@@ -15,3 +15,7 @@ var interval;
 for(var i = 0; i <= 60; i++){
     minutos.innerHTML += '<option value="' +i+ '">' +i+ ' </option>';
 }
+
+for(var i = 1; i <= 60; i++){
+    segundos.innerHTML += '<option value="' +i+ '">' +i+ ' </option>';
+}
